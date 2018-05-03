1c77a7eb822ab081a0e5e1449828c640f3e9d68c
akshayshenoy38 <akshayshenoy38@gmail.com>
2018-05-03 09:56:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/akshay/fragmenttofragactivitycif/FragmentA.java
@@ -0,0 +1,63 @@
+package com.example.akshay.fragmenttofragactivitycif;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FragmentA extends Fragment {
+    private FragmentAListener listener;
+    private EditText et;
+    private Button btnOk;
+
+    public interface FragmentAListener{
+        void onInputASent(CharSequence input);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_a, container, false);
+
+        et = view.findViewById(R.id.et);
+        btnOk = view.findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence input = et.getText();
+                listener.onInputASent(input);
+            }
+        });
+
+        return view;
+
+    }
+
+    public void updateEditText(CharSequence newText) {
+        et.setText(newText);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FragmentAListener) {
+            listener = (FragmentAListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement FragmentAListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+}
---
+++ app/src/main/java/com/example/akshay/fragmenttofragactivitycif/FragmentB.java
@@ -0,0 +1,63 @@
+package com.example.akshay.fragmenttofragactivitycif;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FragmentB extends Fragment {
+    private FragmentBListener listener;
+    private EditText et;
+    private Button btnOk;
+
+    public interface FragmentBListener{
+        void onInputBSent(CharSequence input);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_b, container, false);
+
+        et = view.findViewById(R.id.et);
+        btnOk = view.findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence input = et.getText();
+                listener.onInputBSent(input);
+            }
+        });
+
+        return view;
+
+    }
+
+    public void updateEditText(CharSequence newText) {
+        et.setText(newText);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FragmentBListener) {
+            listener = (FragmentBListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement FragmentBListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+}
